b5c67c7ae021c842b9744c878d27a8ba501ba236
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-03-21 18:26:27 -0400

    add parser

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import db from './db/db'
+import bodyParser from 'body-parser'
 
 const app = express()
+
+// parse incoming request data
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false })
 
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
