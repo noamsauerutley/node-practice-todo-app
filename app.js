88e584f0ff9d43cc2cb078541f3f190c25f304a1
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-03-21 18:39:05 -0400

    remove deprecated parser, use express instead

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 import express from 'express'
 import db from './db/db'
-import bodyParser from 'body-parser'
 
 const app = express()
 
 // parse incoming request data
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false })
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/api/vi/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      succes: 'false',
+      message: 'title is required'
+    })
+  } else if (!req.body.description){
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    })
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+  db.push(todo)
+  return res.status(201).send({
+    success: 'true', 
+    message: 'todo added successfully',
+    todo
+  })
+})
+  
 
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
